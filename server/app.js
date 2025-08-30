ad218a6b2303282873244d5ba4e41bc3670b4b60
gitApurv <apurvmaurya5@gmail.com>
2025-08-30 22:19:16 +0530

    Remove package.json, update Dockerfile to use specific Node version, add MongoDB connection log in app.js, and create docker-compose.yml for client and server services.

--- server/app.js
+++ server/app.js
@@ -38,6 +38,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Connected to MongoDB");
   app.listen(process.env.PORT, () => {
     console.log("Server is running on port", process.env.PORT);
   });
